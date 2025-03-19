4c0b7b95cae58f43796b5205402fecff73a26fcd
ElliotMaringer <e9maringer@gmail.com>
2025-03-19 12:50:25 -0500

    Updated full court code with changes to the three point line, free throw lane and rim/backboard

--- NBACourt.R
+++ NBACourt.R
@@ -1,6 +1,6 @@
 ## This function draws an nba court on ggplot
 
-draw_half_court <- function(line_color = "#000000", court_background = "white") {
+draw_court <- function(line_color = "#000000", court_background = "white") {
   list(
     # Court background (half-court: x from -250 to 250, y from 0 to 470)
     annotate("rect",
@@ -16,16 +16,23 @@ draw_half_court <- function(line_color = "#000000", court_background = "white") 
     
     # Free throw lane
     annotate("rect",
-             xmin = -80, xmax = 80,
-             ymin = 0,   ymax = 190,
+             xmin = -70, xmax = 70,
+             ymin = 0,   ymax = 170,
              fill = NA, color = line_color),
     
-    # Free throw circle (top half)
+    # Free throw circle 
     
     annotate(
       "path",
-      x = 80 * cos(seq(0, 2*pi, length.out = 200)),
-      y = 190 + 80 * sin(seq(0, 2*pi, length.out = 200)),
+      x = 70* cos(seq(0, 2*pi, length.out = 200)),
+      y = 170 +  70* sin(seq(0, 2*pi, length.out = 200)),
+      color = line_color),
+    
+    ##Middle circle
+    annotate(
+      "path",
+      x = 60 * cos(seq(0, 2*pi, length.out = 200)),
+      y = 400 + 60 * sin(seq(0, 2*pi, length.out = 200)),
       color = line_color),
     
     
@@ -33,31 +40,31 @@ draw_half_court <- function(line_color = "#000000", court_background = "white") 
     # Rim (circle at center = (0, 60))
     annotate("path",
              x = 7.5 * cos(seq(0, 2*pi, length.out = 100)),
-             y = 45 + 7.5 * sin(seq(0, 2*pi, length.out = 100)),
+             y = 15 + 7.5 * sin(seq(0, 2*pi, length.out = 100)),
              color = "orange", size = 1.2),
     
     # Backboard (just above the rim)
     annotate("segment",
              x = -30, xend = 30,
-             y = 45,  yend = 45,
+             y = 15,  yend = 15,
              color = line_color, size = 1.5),
     
-    
+    ##Three point line
     
     annotate("segment",
-             x = -230, xend = -230,
-             y = 0,  yend = 70,
+             x = -220, xend = -220,
+             y = 0,  yend = 40,
              color = line_color),
     
     annotate("segment",
-             x = 230, xend = 230,
-             y = 0,  yend = 70,
+             x = 220, xend = 220,
+             y = 0,  yend = 40,
              color = line_color),
     
     annotate(
       "path",
-      x = 230 * cos(seq(0, pi, length.out = 200)),
-      y = 70 + 230 * sin(seq(0, pi, length.out = 200)),
+      x = 220 * cos(seq(0, pi, length.out = 200)),
+      y = 40 + 220 * sin(seq(0, pi, length.out = 200)),
       color = line_color
       
     )
