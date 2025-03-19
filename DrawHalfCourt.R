8b5660ffd5bd096fb507099f5a8f74466299f8bb
ElliotMaringer <e9maringer@gmail.com>
2025-03-19 12:49:26 -0500

    This code draws an NBA half court

---
+++ DrawHalfCourt.R
@@ -0,0 +1,74 @@
+## This function draws an nba court on ggplot
+
+draw_half_court <- function(line_color = "#000000", court_background = "white") {
+  list(
+    # Court background (half-court: x from -250 to 250, y from 0 to 470)
+    annotate("rect",
+             xmin = -250, xmax = 250,
+             ymin = 0,    ymax = 400,
+             fill = court_background, color = line_color),
+    
+    
+    
+    # half court line
+    annotate("segment", x = -250, xend = 250, y = 400,   yend = 400,   color = line_color),
+    
+    
+    # Free throw lane
+    annotate("rect",
+             xmin = -70, xmax = 70,
+             ymin = 0,   ymax = 170,
+             fill = NA, color = line_color),
+    
+    # Free throw circle 
+    
+    annotate(
+      "path",
+      x = 70* cos(seq(0, 2*pi, length.out = 200)),
+      y = 170 +  70* sin(seq(0, 2*pi, length.out = 200)),
+      color = line_color),
+    
+    ##Middle circle
+    annotate(
+      "path",
+      x = 60 * cos(seq(pi, 2*pi, length.out = 200)),
+      y = 400 + 60 * sin(seq(pi, 2*pi, length.out = 200)),
+      color = line_color),
+    
+    
+    
+    # Rim (circle at center = (0, 60))
+    annotate("path",
+             x = 7.5 * cos(seq(0, 2*pi, length.out = 100)),
+             y = 15 + 7.5 * sin(seq(0, 2*pi, length.out = 100)),
+             color = "orange", size = 1.2),
+    
+    # Backboard (just above the rim)
+    annotate("segment",
+             x = -30, xend = 30,
+             y = 15,  yend = 15,
+             color = line_color, size = 1.5),
+    
+    ##Three point line
+    
+    annotate("segment",
+             x = -220, xend = -220,
+             y = 0,  yend = 40,
+             color = line_color),
+    
+    annotate("segment",
+             x = 220, xend = 220,
+             y = 0,  yend = 40,
+             color = line_color),
+    
+    annotate(
+      "path",
+      x = 220 * cos(seq(0, pi, length.out = 200)),
+      y = 40 + 220 * sin(seq(0, pi, length.out = 200)),
+      color = line_color
+      
+    )
+  )
+  
+  
+}
