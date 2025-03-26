0c8f8153f58a076207103c4a4635d757c1c9309d
ElliotMaringer <e9maringer@gmail.com>
2025-03-26 18:31:27 -0500

    Necessary packages and colors for the plots. Example usage of both functions for creating plots.

--- PlotCreation.R
+++ PlotCreation.R
@@ -1,11 +1,14 @@
 #Data Collection and Tidying
-install.packages("devtools")
+
 library(devtools)
 library(nbastatR)
 library(dplyr)
 library(ggplot2)
+library(purrr)
 devtools::install_github("abresler/nbastatR")
 devtools::install_github("abresler/nbastatR", force = TRUE)
+
+Sys.setenv("VROOM_CONNECTION_SIZE" = 500000)
 
 teams <- nba_teams()
 ls("package:nbastatR")
@@ -16,70 +19,15 @@ shots2019 <- teams_shots(teams = "Milwaukee Bucks", team_ids = NULL,
                      measures = "FGA", periods = 0, months = 0, date_from = NA,
                      date_to = NA, nest_data = F, return_message = T)
 
-bucks_green <- "#00471B"
-bucks_cream <- "#EEE1C6"
-halfCourtShots <- filter(shots2019, shots2019$locationY < 400)
-##Half Court Shot Chart
+seasons <- 2015:2019
+allSeasonsBucks <- map_dfr(seasons, ~teams_shots(teams = "Milwaukee Bucks", team_ids = NULL,
+                                                 all_active_teams = F, season_types = "Regular Season", seasons = .x,
+                                                 measures = "FGA", periods = 0, months = 0, date_from = NA,
+                                                 date_to = NA, nest_data = F, return_message = T) %>%
+  mutate(season = .x)
+)
 
-ggplot() +
-  # Draw the court first
-  draw_half_court(line_color = "black", court_background = "white") +
-  # Plot shots
-  geom_point(
-    data = halfCourtShots,
-    aes(x = locationX, y = locationY, color = isShotMade),
-    alpha = 0.1,
-    size = 2
-  ) +
-  coord_fixed() +  # keep aspect ratio consistent
-  scale_color_manual(
-    values = c("FALSE" = "red", "TRUE" = bucks_green),
-    name = "Shot Result"
-  ) +
-  labs(
-    title = "Milwaukee Bucks Shot Locations (2019 Season)",
-    subtitle = "Colored by shot outcome",
-    x = NULL,
-    y = NULL
-  ) +
-  theme_minimal() +
-  theme(
-    legend.position = "bottom",
-    panel.background = element_rect(fill = "white", color = NA),
-    plot.background = element_rect(fill = "white", color = NA)
-  )
+SingleSeasonPlot(shots2019, "Full")
 
-##Full Court Shot Chart
-
-ggplot() +
-  # Draw the court first
-  draw_court(line_color = "black", court_background = "white") +
-  # Plot shots
-  geom_point(
-    data = shots2019,
-    aes(x = locationX, y = locationY, color = isShotMade),
-    alpha = 0.1,
-    size = 2
-  ) +
-  coord_fixed() +  # keep aspect ratio consistent
-  scale_color_manual(
-    values = c("FALSE" = "red", "TRUE" = bucks_green),
-    name = "Shot Result"
-  ) +
-  labs(
-    title = "Milwaukee Bucks Shot Locations (2019 Season)",
-    subtitle = "Colored by shot outcome",
-    x = NULL,
-    y = NULL
-  ) +
-  theme_minimal() +
-  theme(
-    legend.position = "bottom",
-    panel.background = element_rect(fill = "white", color = NA),
-    plot.background = element_rect(fill = "white", color = NA)
-  )
-
-ggplot() +
-  draw_half_court()
 
 
