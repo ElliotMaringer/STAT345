ed09892fcf0013fc11dbdf9f097feed59583a88f
ElliotMaringer <e9maringer@gmail.com>
2025-03-26 18:32:56 -0500

    Example usage and necessary libraries

--- PlotCreation.R
+++ PlotCreation.R
@@ -1,33 +1,25 @@
-#Data Collection and Tidying
 
 library(devtools)
 library(nbastatR)
 library(dplyr)
 library(ggplot2)
 library(purrr)
+library(gganimate)
 devtools::install_github("abresler/nbastatR")
 devtools::install_github("abresler/nbastatR", force = TRUE)
 
 Sys.setenv("VROOM_CONNECTION_SIZE" = 500000)
 
-teams <- nba_teams()
-ls("package:nbastatR")
+
+bucks_green <- "#00471B"
+bucks_cream <- "#EEE1C6"
 
 
-shots2019 <- teams_shots(teams = "Milwaukee Bucks", team_ids = NULL,
-                     all_active_teams = F, season_types = "Regular Season", seasons = 2019,
-                     measures = "FGA", periods = 0, months = 0, date_from = NA,
-                     date_to = NA, nest_data = F, return_message = T)
+SingleSeasonPlot(shots2015, "Half")
+AnimationCreation(2000, 2019)
 
-seasons <- 2015:2019
-allSeasonsBucks <- map_dfr(seasons, ~teams_shots(teams = "Milwaukee Bucks", team_ids = NULL,
-                                                 all_active_teams = F, season_types = "Regular Season", seasons = .x,
-                                                 measures = "FGA", periods = 0, months = 0, date_from = NA,
-                                                 date_to = NA, nest_data = F, return_message = T) %>%
-  mutate(season = .x)
-)
 
-SingleSeasonPlot(shots2019, "Full")
+
 
 
 
